d16f683bc45b7aac59d1142fe8265eee160e9fd4
Grog <dorian.b24@gmail.com>
2026-01-18 17:11:41 -0400

    Update config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    description:"Documentation for Your Project",
+  name:"RPGML",
+  latestVersion:"Version",
+  otherVersions:null,
+  favicon:"assets/docsIcon.png",
+  themeColor:"#FFFFFF",
+};
